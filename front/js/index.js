2f1d18038f0195419102c6f822a923fab0145633
DuvauxMaxime <devpro.dvx@gmail.com>
2022-12-27 10:15:56 +0100

    CI(index.js) - Import canapés ok

--- front/js/index.js
+++ front/js/index.js
@@ -5,7 +5,26 @@ fetch("http://localhost:3000/api/products") // Récup. données = méthode fetch
         }
     })
     .then(function (value) { //Récupération de la valeur 
-        console.log(value);
+        let listeCanapes = "";
+        value.forEach((canape) => {
+            listeCanapes += `<a href="./product.html?id=${canape._id}">
+            <article>
+              <img src="${canape.imageUrl}" alt="${canape.altTxt}">
+              <h3 class="productName">${canape.name}</h3>
+              <p class="productDescription">${canape.description}</p>
+            </article>
+          </a>`;
+        });
+        // for (canape of value) { //Boucle pour créer chaque item canapé
+        //     listeCanapes += `<a href="./product.html?id=${canape._id}">
+        //     <article>
+        //       <img src="${canape.imageUrl}" alt="${canape.altTxt}">
+        //       <h3 class="productName">${canape.name}</h3>
+        //       <p class="productDescription">${canape.description}</p>
+        //     </article>
+        //   </a>`;
+        // }
+        document.querySelector('#items').innerHTML = listeCanapes; //Insertion des items dans notre document
     })
     .catch(function (err) { //Retour en cas d'erreur
     });
